05f4aae35ecee27ac204c3a8340d91a71e0b6d15
JMiller686 <jmiller686@gmail.com>
2019-03-22 09:58:54 -0500

    installed babel-core and babel-loader, setup presets in webpack.config

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 //install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const template = React.createElement('p',null,'testing 123');
+
+ReactDOM.render(template, document.getElementById('app'));
