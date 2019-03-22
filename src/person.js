fa9f727fc99f22ec0f1b60c960cfa0a9be8d061b
JMiller686 <jmiller686@gmail.com>
2019-03-22 09:03:32 -0500

    mrefactored functions in person.js

--- src/person.js
+++ src/person.js
@@ -1,5 +1,5 @@
-const isAdult = (age) => age >= 18 ? true : false;
+const isAdult = (age) => age >= 18;
 
-const canDrink = (age) => age < 21 ? false : true;
+const canDrink = (age) => age > 21;
 
 export { isAdult, canDrink };
