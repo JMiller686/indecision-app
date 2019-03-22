27601596637f4f016ea33c8ff7458f93d65c50c2
JMiller686 <jmiller686@gmail.com>
2019-03-22 11:27:51 -0500

    moved IndecsionApp component directly into the render function

--- src/app.js
+++ src/app.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-<IndecisionApp />
-
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
