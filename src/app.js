f13dcbf0ad116fe12a55533ca63bdcbd81d6fa17
JMiller686 <jmiller686@gmail.com>
2019-03-22 09:27:12 -0500

    practiced import/export

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
-// import { square, add } from './utils.js';
+// import subtract, { square, add } from './utils.js';
 
 // console.log(square(4));
 // console.log(add(100,23));
+// console.log(subtract(100, 50));
 
 
-// person.js
-// named export isAdult(age) return true if adult false otherwise
-// named export canDrink(age) return true if over 21 otherwise false
 
-//import isAdult, canDrink
-// use both, printing results to the console
-
-import { isAdult, canDrink } from './person.js';
+import isSenior, { isAdult, canDrink } from './person.js';
 
 console.log('isAdult', isAdult(19));
 console.log('canDrink', canDrink(19));
+console.log('isSenior', isSenior(65));
