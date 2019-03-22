03e95ba655e5c66ff1931daaca2be6e4e5fd9f77
JMiller686 <jmiller686@gmail.com>
2019-03-22 09:33:53 -0500

    added validator module

--- src/app.js
+++ src/app.js
@@ -1,13 +1,1 @@
-// import subtract, { square, add } from './utils.js';
-
-// console.log(square(4));
-// console.log(add(100,23));
-// console.log(subtract(100, 50));
-
-
-
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log('isAdult', isAdult(19));
-console.log('canDrink', canDrink(19));
-console.log('isSenior', isSenior(65));
+//install -> import -> use
