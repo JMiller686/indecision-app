b9ec486b5f79cba8bad5f9b5f194b509cd97f21d
JMiller686 <jmiller686@gmail.com>
2019-03-23 07:36:46 -0500

    added sass-loader and node-sass modules and added to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,13 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/
         }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+
         }]
     },
     devtool: 'cheap-module-eval-source-map',
