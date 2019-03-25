f965dc58ba77ec3077a3af197f488d05de0ec60e
JMiller686 <jmiller686@gmail.com>
2019-03-25 13:59:45 -0500

    added modal partial and styled the modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
     <Modal 
         isOpen={!!props.selectedOption} 
         contentLabel="Selected Option"
-        onRequestClose={props.handleClearSelected}>
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelected}>close</button>
+        onRequestClose={props.handleClearSelected}
+        closeTimeoutMS={200}
+        className="modal">
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelected}>close</button>
     </Modal>
 );
 
